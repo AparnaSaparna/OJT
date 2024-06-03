9fd33d03d5fd22d04b7da9971c443d66b7b0d5c8
AparnaSaparna <122439888+AparnaSaparna@users.noreply.github.com>
2024-06-03 16:47:32 +0530

    Delete 01-06-2024 directory

--- 01-06-2024/AddNo.java
+++
@@ -1,8 +0,0 @@
-public class AddNo {
-    public static void main(String[] args) {
-        int a=60,b=90;
-        int c=a+b;
-        System.out.println(c);
-    }
-    
-}
--- 01-06-2024/SumArray.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-public class SumArray {
-
-    public static void main(String[] args) {
-    int [] arr={2,4,5,6,7};
-    System.out.println(Arrays.stream(arr).sum());
-    }
-}
